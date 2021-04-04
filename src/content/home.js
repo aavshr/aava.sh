10f74413778d9d407221d57d1af1f39a29553bbd
Aavash Shrestha <aavash09@gmail.com>
2021-04-04 16:45:21 +0200

    basic commands

---
+++ src/content/home.js
@@ -0,0 +1,10 @@
+export const files = {
+    "about.txt": [
+        "Hello, I am Aavash Shrestha. I currently work as a software engineer at Deta (https://web.deta.sh).",
+        "-----",
+        "Deta gifted me this .sh domain and which gave me an idea, what if I wrote a terminal as my personal website.",
+        "I thought it would be fun and something unique.",
+        "------", 
+        "If you experience any bugs, please file an issue in the github repo (https://github.com/aavshr/aava.sh)."
+    ],
+}
--- src/helpers/utils.js
+++ src/helpers/utils.js
@@ -1,6 +1,10 @@
+import { uuid } from 'uuidv4';
+
 export const keys = {
     KEY_RETURN : 13,
     KEY_UP : 38, 
     KEY_DOWN: 40,
     KEY_ESC: 27
 };
+
+export const genUuid = uuid;
