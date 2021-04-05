b0da15e6eccd16513c24d90894478c4dc29bb481
Aavash Shrestha <aavash09@gmail.com>
2021-04-05 20:35:05 +0200

    run execs with dot slash

--- src/helpers/commands/parser.js
+++ src/helpers/commands/parser.js
@@ -1,10 +1,24 @@
 const parseCommand = (cmd) => {
     const parts = cmd.toLowerCase().trim().replace(/ +/g, ' ').split(' '); 
-    const command = parts[0];
+    let command = parts[0];
     let args = [];
+
     if (parts.length > 1){
         args = parts.slice(1, parts.length);
     }
+
+    if (command.startsWith("./")){
+        command = "sh";
+        const dotSlashArgs = parts[0].slice(2)
+        if (dotSlashArgs.length > 1){
+            args.unshift(dotSlashArgs)
+        }
+        return {
+            command: command,
+            args: args,
+        }
+    } 
+
     return {
         command: command, 
         args: args,
