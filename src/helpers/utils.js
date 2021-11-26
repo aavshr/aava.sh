d50a19b372ceb32d81d8c217223acaadb5c3c32e
Aavash Shrestha <aavshr@gmail.com>
2021-11-26 19:59:48 +0100

    src/helpers: move splitting command to utils

--- src/helpers/utils.js
+++ src/helpers/utils.js
@@ -4,7 +4,8 @@ export const keys = {
     KEY_RETURN : 13,
     KEY_UP : 38, 
     KEY_DOWN: 40,
-    KEY_ESC: 27
+    KEY_ESC: 27,
+    KEY_TAB: 9
 };
 
 export const genUuid = v4;
@@ -13,4 +14,8 @@ export const fileType = {
     regular: 0, 
     exec: 1,
     dir: 2,
+}
+
+export const getCommandParts = (cmd) => {
+    return cmd.toLowerCase().trim().replace(/ +/g, ' ').split(' '); 
 }
