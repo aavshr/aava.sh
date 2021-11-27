0f97812409c6ca99d28b821ee167ebcb72fcdccb
Aavash Shrestha <aavash09@gmail.com>
2021-11-27 13:48:49 +0100

    src/content/home: update about info

--- src/content/home.js
+++ src/content/home.js
@@ -7,9 +7,9 @@ export const files = {
     "about.txt": {
         name: "about.txt",
         content: [
-            "Hello, I am Aavash. I currently work as a software engineer at Deta (https://web.deta.sh).",
+            "Hello, I am Aavash. I currently work as a software engineer at Deta (https://deta.sh).",
             "-----",
-            "Deta gifted me this '.sh' domain which gave me an idea, what if I wrote a shell as my personal website.",
+            "Deta gifted me this '.sh' domain which gave me the idea of writing a shell as my personal website.",
             "I thought it would be fun and something unique.",
             "------", 
             "If you experience any bugs, please file an issue in the github repo (https://github.com/aavshr/aava.sh)."
