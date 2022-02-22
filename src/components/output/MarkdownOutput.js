67f91da64d94dc7959993136e63258c164f0d6b6
Aavash Shrestha <aavshr@gmail.com>
2022-02-22 23:02:03 +0100

    add support for markdown content

---
+++ src/components/output/MarkdownOutput.js
@@ -0,0 +1,35 @@
+import styled from '@emotion/styled';
+import {useState, useEffect} from 'react';
+import Markdown from 'markdown-to-jsx';
+
+import { regularTextStyle } from '../../styles/_typographies';
+
+const MarkdownOutputContainer = styled.div`
+    color: ${(props) => props.theme.colors.white};
+    display: flex;
+    padding-left: 10px;
+    align-items: center;
+    ${regularTextStyle};
+`;
+
+export default function MarkdownOutput({path}){
+    const [content, setContent] = useState('');
+
+    useEffect(() => {
+        import(`../../content/${path}`)
+            .then(md => {
+                fetch(md.default)
+                    .then(res => res.text())
+                    .then(text => setContent(text))
+            })
+            .catch(err => console.log(err));
+    });
+
+    return (
+        <MarkdownOutputContainer>
+            <Markdown>
+                {content}
+            </Markdown>
+        </MarkdownOutputContainer>
+    );
+}
---
+++ src/content/markdown/about.md
@@ -0,0 +1,8 @@
+Hello 👋, I am Aavash. 
+
+I am an engineer at <span style="color:#f478b3"> <a href="https://www.deta.sh" target="_blank">Deta</a></span>.
+
+Deta gifted me this `.sh` domain which gave me the idea of writing a shell
+as my personal website, thought it would be something fun.
+
+The source code is <span style="color:#fdf0d5"><a href="https://github.com/aavshr/aava.sh" target="_blank">available in Github</a></span> if anyone wants to fork and stuff.
